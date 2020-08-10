0233afa2a16995d18f565b984380cb84e66a0f17
Sky <mying4@wisc.edu>
2020-08-10 16:04:50 -0500

    scGRN_getNt update

--- R/scGRN_getNt.R
+++ R/scGRN_getNt.R
@@ -220,7 +220,8 @@ scGRN_getNt <- function(df, gexpr, df_gene_id = 'hgnc_symbol', gexpr_gene_id = '
     df <- data.frame(TG = NULL, TF = NULL, enhancer = NULL, promoter = NULL, TFbs = NULL,
                     coef = NULL)
   }
-
+ 
+  print('hello')
   return(df)
 
 }
